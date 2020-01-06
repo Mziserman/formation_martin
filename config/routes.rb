d4dc8ea39d366b8cfdbc89a863af6ed52358291d
MZiserman <martinziserman@gmail.com>
2020-01-06 15:20:31 +0100

    setup letter opener

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
   root to: 'application#index'
 end
