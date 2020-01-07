6ef8ec37fe1f06f30d4646c7202f9f6beceec0d6
MZiserman <martinziserman@gmail.com>
2020-01-07 15:10:32 +0100

    overwrite sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # def create
-  #   user_transaction = User::CreateTransaction.new
-  #   user_transaction.call(sign_up_params: sign_up_params) do |transaction|
-  #     transaction.success do |user|
-  #     end
+  def create
+    user_transaction = Users::CreateTransaction.new
+    user_transaction.call(sign_up_params: sign_up_params) do |transaction|
+      transaction.success do |user|
+        sign_in(user)
+        redirect_to root_path
+      end
 
-  #     transaction.failure :create do |user|
-  #     end
-
-  #     transaction.failure :send_welcome_email do |error|
-  #     end
-  #   end
-  # end
+      transaction.failure do |user|
+        user.password = ''
+        user.password_confirmation = ''
+        render :new, locals: { resource: user }
+      end
+    end
+  end
 end
