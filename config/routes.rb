88b21e7c6e9bf38609968b355b544d7fabbe7865
MZiserman <martinziserman@gmail.com>
2020-01-08 15:53:12 +0100

    move routes from application to home controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   }
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
-  root to: 'application#index'
+  root to: 'home#index'
   get 'dashboard', to: 'dashboard#index'
 end
--- spec/support/config/devise.rb
+++ spec/support/config/devise.rb
@@ -1,6 +1,8 @@
 require 'devise'
+require_relative '../helpers/devise_controller_helpers'
 
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Warden::Test::Helpers, type: :controller
+  config.extend DeviseControllerHelpers, type: :controller
 end
