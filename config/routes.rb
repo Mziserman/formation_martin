4fc75ee892241a57067e99f9828e1b8885a22969
MZiserman <martinziserman@gmail.com>
2020-01-08 15:31:13 +0100

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   root to: 'application#index'
+  get 'dashboard', to: 'dashboard#index'
 end
