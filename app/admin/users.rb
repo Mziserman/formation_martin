be4f718749db214f9566b7663002da6a72d34201
MZiserman <martinziserman@gmail.com>
2020-01-09 17:44:14 +0100

    start user backoffice

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,18 +1,44 @@
 ActiveAdmin.register User do
+  permit_params :email,
+                :first_name,
+                :last_name,
+                :birthdate
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :first_name, :last_name, :birthdate
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :first_name, :last_name, :birthdate]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  # ID
+  # Email
+  # Nom
+  # Prénom
+  # Date de création
+  # Date de derniere edition
+  # Nombre de connexion
+  # Derniere connexion
+  # Adresse IP
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :first_name
+    column :last_name
+    column :current_sign_in_at
+    column :last_ip_address
+    column :sign_in_count
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  filter :email
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 end
