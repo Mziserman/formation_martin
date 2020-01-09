c048fd160fae6653ae5c1e92fd35fdcccc97a842
MZiserman <martinziserman@gmail.com>
2020-01-09 15:37:31 +0100

    use block not send

--- app/admin/login_activities.rb
+++ app/admin/login_activities.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register LoginActivity do
   menu false
   # https://github.com/activeadmin/activeadmin/issues/221#issuecomment-10757256
-  controller.send(:belongs_to, :admin_user, :user, polymorphic: true)
+  controller do
+    belongs_to :admin_user, :user, polymorphic: true
+  end
 end
