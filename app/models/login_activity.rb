f0d13536181fd49334b737dd21230412d7f59bff
MZiserman <martinziserman@gmail.com>
2020-01-10 10:23:53 +0100

    more logical method name

--- app/models/login_activity.rb
+++ app/models/login_activity.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class LoginActivity < ApplicationRecord
   belongs_to :user, polymorphic: true, optional: true
 
-  after_create :update_user_counter_cache_and_last_connected_at, if: :success?
-  after_destroy :update_user_counter_cache_and_last_connected_at, if: :success?
+  after_create :update_user_successfull_login_activities_count_and_last_connected_at, if: :success?
+  after_destroy :update_user_successfull_login_activities_count_and_last_connected_at, if: :success?
 
-  def update_user_counter_cache_and_last_connected_at
+  def update_user_successfull_login_activities_count_and_last_connected_at
     user.update_successfull_login_activities_count_and_last_connected_at
   end
 end
