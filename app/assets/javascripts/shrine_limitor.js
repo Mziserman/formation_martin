23e712c01f41636b9200b7e6340d34b79efed572
MZiserman <martinziserman@gmail.com>
2020-01-13 09:55:06 +0100

    rename unused arg

--- app/assets/javascripts/shrine_limitor.js
+++ app/assets/javascripts/shrine_limitor.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
   $add_button.click((_) => {
     $add_button.hide();
-    $.when($container.find('a.button.has_many_remove')).then((self) => {
+    $.when($container.find('a.button.has_many_remove')).then((_) => {
       $container.find('a.button.has_many_remove').click((_) => {
         $add_button.show();
       })
