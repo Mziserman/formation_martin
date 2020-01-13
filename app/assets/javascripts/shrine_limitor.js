6e70ddf0692be8c89e4da05307099fc87da6fadf
MZiserman <martinziserman@gmail.com>
2020-01-13 09:53:59 +0100

    allow only one picture

---
+++ app/assets/javascripts/shrine_limitor.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  let $container = $('.has_many_container.thumbnail');
+  let $add_button = $container.find('a.button.has_many_add');
+
+  $add_button.click((_) => {
+    $add_button.hide();
+    $.when($container.find('a.button.has_many_remove')).then((self) => {
+      $container.find('a.button.has_many_remove').click((_) => {
+        $add_button.show();
+      })
+    });
+  })
+})
--- app/models/project.rb
+++ app/models/project.rb
@@ -8,9 +8,9 @@ class Project < ApplicationRecord
             presence: true
 
   belongs_to :thumbnail, class_name: 'Photo', optional: true
-  accepts_nested_attributes_for :thumbnail
+  accepts_nested_attributes_for :thumbnail, allow_destroy: true
   belongs_to :landscape, class_name: 'Photo', optional: true
-  accepts_nested_attributes_for :landscape
+  accepts_nested_attributes_for :landscape, allow_destroy: true
 
   has_many :project_ownerships
   has_many :owners, through: :project_ownerships, source: :user
