68cca6727d231cedff8cd3508e5e311e299d450e
MZiserman <martinziserman@gmail.com>
2020-01-13 14:13:47 +0100

    pass images in projects

--- app/admin/projects.rb
+++ app/admin/projects.rb
@@ -6,23 +6,25 @@ ActiveAdmin.register Project do
                 :long_blurb,
                 :amount_wanted_in_cents,
                 :category_list,
-                thumbnail_attributes: %i[id image _destroy],
-                landscape_attributes: %i[id image _destroy]
+                :thumbnail,
+                :landscape
+                # thumbnail_attributes: %i[id image _destroy],
+                # landscape_attributes: %i[id image _destroy]
 
-  form do |f|
-    f.inputs do
-      f.has_many :thumbnail, heading: 'Thumbnail',
-                             allow_destroy: true,
-                             new_record: !f.object.thumbnail.present? do |a|
-        a.input :image, as: :hidden
-        if a.object.image.present?
-          a.input :image, as: :file, hint: image_tag(a.object.image.url)
-        else
-          a.input :image, as: :file, hint: content_tag(:span, 'no cover page yet')
-        end
-      end
-    end
+  # form do |f|
+  #   f.inputs do
+  #     f.has_many :thumbnail, heading: 'Thumbnail',
+  #                            allow_destroy: true,
+  #                            new_record: !f.object.thumbnail.present? do |a|
+  #       a.input :image, as: :hidden
+  #       if a.object.image.present?
+  #         a.input :image, as: :file, hint: image_tag(a.object.image.url)
+  #       else
+  #         a.input :image, as: :file, hint: content_tag(:span, 'no cover page yet')
+  #       end
+  #     end
+  #   end
 
-    f.actions
-  end
+  #   f.actions
+  # end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -7,11 +7,6 @@ class Project < ApplicationRecord
             :amount_wanted_in_cents,
             presence: true
 
-  belongs_to :thumbnail, class_name: 'Photo', optional: true
-  accepts_nested_attributes_for :thumbnail, allow_destroy: true
-  belongs_to :landscape, class_name: 'Photo', optional: true
-  accepts_nested_attributes_for :landscape, allow_destroy: true
-
   has_many :project_ownerships
   has_many :owners, through: :project_ownerships, source: :user
 end
