75d0dc3ee2e6c9f3d3fba6fb7f92effb3a589aa1
MZiserman <martinziserman@gmail.com>
2020-01-14 16:03:18 +0100

    reward bases

---
+++ app/admin/rewards.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Reward do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :project_id, :blurb, :name, :thumbnail, :threshold_in_cents, :limited, :stock, :contributions_count
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:project_id, :blurb, :name, :thumbnail, :threshold_in_cents, :limited, :stock, :contributions_count]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
