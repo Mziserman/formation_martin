fa41d6a2a0c7a66bcc972225a00d5fc63ddeba90
MZiserman <martinziserman@gmail.com>
2020-01-15 11:10:13 +0100

    project show

---
+++ app/helpers/bootstrap_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module BootstrapHelper
+  def type_to_bootstrap(type)
+    HashWithIndifferentAccess.new(
+      info: 'info',
+      notice: 'info',
+      alert: 'danger'
+    )[type] || 'info'
+  end
+
+  def state_to_bootstrap(status)
+    HashWithIndifferentAccess.new(
+      ongoing: 'primary',
+      upcoming: 'secondary',
+      success: 'success',
+      failure: 'danger',
+      draft: 'info'
+    )[status] || 'info'
+  end
+end
