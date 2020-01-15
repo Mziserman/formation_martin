43aa796eda195a9d7f0dae7ae304e2fb31b6eaa3
MZiserman <martinziserman@gmail.com>
2020-01-15 12:14:05 +0100

    fix sum symbol not method

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,6 @@ class User < ApplicationRecord
   end
 
   def amount_contributed_to(project)
-    contributions.where(project_id: project.id).sum(amount)
+    contributions.where(project_id: project.id).sum(:amount)
   end
 end
