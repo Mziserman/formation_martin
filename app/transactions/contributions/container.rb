7d36b29285c61fe2dbb37bd3c8afbc60dc56901e
MZiserman <martinziserman@gmail.com>
2020-01-19 10:10:30 +0100

    contribution container

--- app/transactions/contributions/container.rb
+++ app/transactions/contributions/container.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Projects::Container
+class Contributions::Container
   extend Dry::Container::Mixin
 
   namespace 'contributions' do
--- app/transactions/contributions/create_transaction.rb
+++ app/transactions/contributions/create_transaction.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Contributions::CreateTransaction
-  include Dry::Transaction(container: Projects::Container)
+  include Dry::Transaction(container: Contributions::Container)
 
-  step :add_current_user_to_params, with: 'projects.add_current_user_to_params'
-  step :add_project_to_params, with: 'projects.add_project_to_params'
-  step :create, with: 'projects.create'
+  step :add_current_user_to_params, with: 'contributions.add_current_user_to_params'
+  step :add_project_to_params, with: 'contributions.add_project_to_params'
+  step :create, with: 'contributions.create'
 end
