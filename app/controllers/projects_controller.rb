9604f22410ad818a011e381aa7646d838721182e
MZiserman <martinziserman@gmail.com>
2020-01-19 10:18:57 +0100

    secure show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,18 +12,24 @@ class ProjectsController < ApplicationController
   private
 
   def set_project
-    @project = Project.find(params[:id])
+    @project = if current_admin_user
+                 Project.find(params[:id])
+               else
+                 Project.visible_by_users.find_by(id: params[:id])
+               end
   end
 
   def set_projects
     @projects = if current_admin_user
-                 Project.all
-               else
-                 Project.visible_by_users
-               end
+                  Project.all
+                else
+                  Project.visible_by_users
+                end
   end
 
   def authorize_admin!
+    return redirect_to root_path unless @project.present?
+
     super if @project.draft?
   end
 end
