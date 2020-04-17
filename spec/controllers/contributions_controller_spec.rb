6fec2b8b4b27e5a3ad1d94e4d1a0ec54197d1777
MZiserman <martinziserman@gmail.com>
2020-04-17 10:13:48 +0200

    refacto contribution specs by method

--- spec/controllers/contributions_controller_spec.rb
+++ spec/controllers/contributions_controller_spec.rb
@@ -1,35 +1,37 @@
 # frozen_string_literal: true
 
-describe ContributionsController, type: :controller do
+describe ContributionsController, '#new', type: :controller do
   let(:project) { create :project }
   let(:attributes) { attributes_for :contribution }
-  context 'new not logged' do
-    subject do
-      get :new, params: { project_id: project.id }
-    end
-    it { should redirect_to root_url }
+
+  subject do
+    get :new, params: { project_id: project.id }
   end
 
-  context 'create not logged' do
-    subject do
-      put :create, params: { project_id: project.id }
-    end
+  context 'new not logged' do
     it { should redirect_to root_url }
   end
 
   context 'new logged' do
     login_user
-    subject do
-      get :new, params: { project_id: project.id }
-    end
     it { should render_template :new }
+  end
+end
+
+describe ContributionsController, '#create', type: :controller do
+  let(:project) { create :project }
+  let(:attributes) { attributes_for :contribution }
+
+  subject do
+    put :create, params: { project_id: project.id, contribution: attributes }
+  end
+
+  context 'create not logged' do
+    it { should redirect_to root_url }
   end
 
   context 'create logged with params' do
     login_user
-    subject do
-      put :create, params: { project_id: project.id, contribution: attributes }
-    end
     it 'creates a contribution' do
       expect { subject }.to change(Contribution, :count).by(1)
     end
