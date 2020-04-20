d5d1d929d4aa1249f0466af4342339c945c3c5c5
MZiserman <martinziserman@gmail.com>
2020-04-20 14:50:55 +0200

    cop vcr config

--- spec/support/config/vcr.rb
+++ spec/support/config/vcr.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 require 'vcr'
 
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = 'fixtures/vcr_cassettes'
   config.hook_into :webmock
 end
-
