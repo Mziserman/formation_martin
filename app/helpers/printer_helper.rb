6f43658297ede5650458c57bf809477ee96e0097
MZiserman <martinziserman@gmail.com>
2020-04-21 17:40:38 +0200

    filter projects by aasm state

--- app/helpers/printer_helper.rb
+++ app/helpers/printer_helper.rb
@@ -8,4 +8,13 @@ module PrinterHelper
   def percent_print(ratio)
     number_to_percentage(ratio * 100, delimiter: ' ', precision: 2)
   end
+
+  def aasm_state_choice
+    if current_admin_user.present?
+      [['Brouillon', :draft], ['Prévu', :upcoming], ['En cours', :ongoing],
+       ['Terminé', :success], ['Remboursé', :failure]]
+    else
+      [['En cours', :ongoing], ['Terminé', :success], ['Remboursé', :failure]]
+    end
+  end
 end
