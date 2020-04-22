3d4db0f700da5af949f2070601732d4d5d937ae4
MZiserman <martinziserman@gmail.com>
2020-04-22 11:46:26 +0200

    fix active admin style leaking

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.precompile += ['active_admin.css']
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
