f2f475c880b76386c447d7c479b47b61c1e17111
MZiserman <martinziserman@gmail.com>
2020-04-23 19:21:20 +0200

    use one vcr per subject

--- spec/controllers/contributions_controller_spec.rb
+++ spec/controllers/contributions_controller_spec.rb
@@ -21,36 +21,33 @@ end
 describe ContributionsController, '#create', type: :controller do
   let(:owner) do
     admin_user = create(:admin_user)
-    VCR.use_cassette('create_mangopay_user') do
-      admin_user.mangopay_id
-    end
     admin_user
   end
   let(:project) do
-    VCR.use_cassette('create_mangopay_wallet') do
-      resource = build(:project)
-      resource.owners << owner
+    resource = build(:project)
+    resource.owners << owner
 
-      Projects::CreateTransaction.new.call(resource: resource).success[:resource]
-    end
+    resource
   end
 
   let(:attributes) { attributes_for(:contribution, amount: rand(100..100_000_000)) }
 
   subject do
-    put :create, params: { project_id: project.id, contribution: attributes }
-  end
+    VCR.use_cassette('create_mangopay_payin_card_web', allow_playback_repeats: true) do
+      Projects::CreateTransaction.new.call(resource: project)
 
-  context 'create not logged' do
-    it { should redirect_to root_url }
+      put :create, params: { project_id: project.id, contribution: attributes }
+    end
   end
 
   context 'create logged with params' do
     login_user
     it 'creates a contribution' do
-      VCR.use_cassette('create_mangopay_payin_card_web') do
-        expect { subject }.to change(Contribution, :count).by(1)
-      end
+      expect { subject }.to change(Contribution, :count).by(1)
     end
+  end
+
+  context 'create not logged' do
+    it { should redirect_to root_url }
   end
 end
--- spec/transactions/projects/create_transaction_spec.rb
+++ spec/transactions/projects/create_transaction_spec.rb
@@ -1,33 +1,28 @@
 # frozen_string_literal: true
 
-RSpec.describe Contributions::CreateTransaction do
+RSpec.describe Projects::CreateTransaction do
   subject do
-    project.owners << owner
-    Projects::CreateTransaction.new.call(resource: project)
+    VCR.use_cassette('create_mangopay_wallet') do
+      project.owners << owner
+      Projects::CreateTransaction.new.call(resource: project)
+    end
   end
 
   context 'with valid attributes' do
     let(:owner) do
       admin_user = create(:admin_user)
-      VCR.use_cassette('create_mangopay_user') do
-        admin_user.mangopay_id
-      end
+      admin_user.mangopay_id
       admin_user
     end
 
     let(:project) { build(:project) }
 
     it 'gets mangopay payin id' do
-      VCR.use_cassette('create_mangopay_wallet') do
-        subject
-      end
-
+      subject
       expect(Project.last.mangopay_wallet_id).to_not eq nil
     end
     it 'creates a project' do
-      VCR.use_cassette('create_mangopay_wallet') do
-        expect { subject }.to change { Project.count }.by(1)
-      end
+      expect { subject }.to change { Project.count }.by(1)
     end
   end
 end
