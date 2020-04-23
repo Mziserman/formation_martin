c46c093aa7468fc35799039716da70fb2ca703ea
MZiserman <martinziserman@gmail.com>
2020-04-23 11:17:16 +0200

    fix project seeds, create user account

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,11 @@ if Rails.env.development?
   3.times do
     project = build(
       :project,
-      :with_categories,
-      :with_rewards,
-      owners: owners.sample(rand(1..3)),
-      categories: categories.sample(rand(1..3))
+      :with_rewards
     )
+    project.owners = owners.sample(rand(1..3))
+    project.category_list.add(*categories.sample(rand(1..3)))
+
     Projects::CreateTransaction.new.call(resource: project)
   end
 
@@ -22,6 +22,14 @@ if Rails.env.development?
     email: 'admin@gmail.com',
     password: 'password',
     password_confirmation: 'password',
+    first_name: 'Martin',
+    last_name: 'Ziserman',
+    birthdate: Date.new(1993, 2, 22)
+  )
+  User.create!(
+    email: 'martinziserman@gmail.com',
+    password: 'AZERTYUIOP',
+    password_confirmation: 'AZERTYUIOP',
     first_name: 'Martin',
     last_name: 'Ziserman',
     birthdate: Date.new(1993, 2, 22)
