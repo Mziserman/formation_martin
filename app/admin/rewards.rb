23c18d378fa2954b350cdcf1279e260685bdd152
MZiserman <martinziserman@gmail.com>
2020-04-23 10:03:47 +0200

    fix rewards creation

--- app/admin/rewards.rb
+++ app/admin/rewards.rb
@@ -1,18 +1,8 @@
 ActiveAdmin.register Reward do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :project_id, :blurb, :name, :thumbnail, :threshold, :limited, :stock, :contributions_count
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:project_id, :blurb, :name, :thumbnail, :threshold, :limited, :stock, :contributions_count]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-
+  permit_params :name,
+                :project_id,
+                :blurb,
+                :threshold,
+                :limited,
+                :total_stock
 end
