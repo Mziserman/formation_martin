56326eaf6f78091599f4d5cd0a34263c418ff61a
MZiserman <martinziserman@gmail.com>
2020-04-27 10:13:06 +0200

    try

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,10 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    respond_to do |format|
+      format.html {}
+      format.pdf do
+        render pdf: "file_name"   # Excluding ".pdf" extension.
+      end
+    end
+  end
 end
